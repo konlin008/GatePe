8f8a9074676bc76106fe36ade53ca7adcbcd36c2
Aman Mondal <162113539+konlin008@users.noreply.github.com>
2025-07-10 23:53:57 +0530

    Register controller

--- server/server.js
+++ server/server.js
@@ -7,6 +7,8 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
+app.use("/api/v1/auth/", authRouter);
+
 app.listen(8080, () => {
   console.log("Server is Running");
 });
--- server/src/controllers/auth.controller.js
+++ server/src/controllers/auth.controller.js
@@ -1,11 +1,37 @@
 import bcrypt from "bcrypt";
+import prisma from "../../prisma/config/prismaClient.js";
 
 export const register = async (req, res) => {
-  const { email, firstName, lastName, password } = req.body;
-  if (!email || firstName || lastName || password)
-    return res.status(400).json({
-      msg: "All fields are Required",
+  try {
+    const { email, firstName, lastName, password } = req.body;
+    if (!email || !firstName || !lastName || !password)
+      return res.status(400).json({
+        message: "All fields are Required",
+      });
+    const existingUser = await prisma.user.findUnique({
+      where: {
+        email,
+      },
     });
-  const hashedPassword = bcrypt.hash(password, 10);
-  
+    if (existingUser)
+      return res.status(400).json({ message: "User Alredy Registered" });
+    const hashedPassword = await bcrypt.hash(password, 10);
+    const newUser = await prisma.user.create({
+      data: {
+        firstName,
+        lastName,
+        email,
+        password: hashedPassword,
+      },
+    });
+    if (newUser)
+      return res
+        .status(200)
+        .json({ message: "User Registered Successfully", success: true });
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({
+      message: "Something Went Wrong Internal Server",
+    });
+  }
 };
