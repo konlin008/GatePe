29d545b1d432c0cc2fdf2befc11b35bab0d70c54
Aman Mondal <162113539+konlin008@users.noreply.github.com>
2025-07-15 23:23:34 +0530

    auth0 integration

---
+++ client/src/layout/MainLayout.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const MainLayOut = () => {
+    return (
+        <div className='flex flex-col min-h-screen'>
+            <h1>main</h1>
+        </div>
+    )
+}
+
+export default MainLayOut
